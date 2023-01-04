54e3087ea5bb7cbe09170e43a79442c55f49c2c7
Navyashree0923 <navyashree0923@gmail.com>
2023-01-04 15:46:38 +0530

    NodeJs Environment Setup

--- .github/workflows/scripts/grumpy_pig.js
+++ .github/workflows/scripts/grumpy_pig.js
@@ -1,14 +1,6 @@
-/*
- * All this program does is print
- * "I am a lazy pig for 1000 times"
- */
-
-
-
 function lazy_pig(){
-	// Say i am a lazy pig here
 	for (let i = 1; i <= 1000; i++) {
-		console.log(String(i) + '|' + "I AM A GRUMPY 25 YEAR OLD PIG, OINK, OINK, OINK ...")
+		console.log(String(i) + '|' + "GRUMPY OLD PIG, OINK, OINK, OINK ...")
 	}
 }
 
